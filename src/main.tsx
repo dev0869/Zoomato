81e665e81097a604aef57f230f1ea03988232672
dev0869 <deveshbisht36@gmail.com>
2024-01-18 17:37:56 +0530

    sda

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { persistStore } from "redux-persist";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const queryClient = new QueryClient({
@@ -16,7 +16,7 @@ const queryClient = new QueryClient({
     },
   },
 });
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
